32016c339b2b153d3197db2e806c2409418d5b7a
tnfo222 <tayforbes92@hotmail.com>
2022-08-07 13:19:32 -0400

    TopBar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TopBar from './Components/TopBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <TopBar />
     </div>
   );
 }
