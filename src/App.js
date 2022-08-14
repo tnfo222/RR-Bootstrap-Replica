4fe7b12613fb78951e7b010cb5978b3cb740d033
tnfo222 <tayforbes92@hotmail.com>
2022-08-14 12:00:49 -0400

    Added background and more javascript to practice bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-import TopBar from './Components/TopBar';
+import TopBar from './components/TopBar'
+import Background from './components/Background'
+import StoreListings from './components/StoreListings'
+
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <Background />
+      <StoreListings />
     </div>
   );
 }
